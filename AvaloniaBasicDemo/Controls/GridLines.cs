baee5602273be625a79694c67dbab9b6c516c921
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-30 14:59:59 +0200

    Move

--- AvaloniaBasicDemo/Controls/GridLines.cs
+++ AvaloniaBasicDemo/Controls/GridLines.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
 
-namespace AvaloniaBasicDemo.Views;
+namespace AvaloniaBasicDemo.Controls;
 
 public sealed class GridLines : Control
 {
