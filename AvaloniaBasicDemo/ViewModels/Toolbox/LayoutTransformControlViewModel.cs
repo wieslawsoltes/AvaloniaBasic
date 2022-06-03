c1148e6e6c92ba4bfdda3b64a7a254bfe66243e3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-03 11:43:55 +0200

    Create LayoutTransformControlViewModel.cs

---
+++ AvaloniaBasicDemo/ViewModels/Toolbox/LayoutTransformControlViewModel.cs
@@ -0,0 +1,37 @@
+using Avalonia.Controls;
+using AvaloniaBasicDemo.Behaviors;
+
+namespace AvaloniaBasicDemo.ViewModels.Toolbox;
+
+public class LayoutTransformControlViewModel : DragItemViewModel
+{
+    public override Control CreatePreview()
+    {
+        var layoutTransformControl = new LayoutTransformControl();
+        layoutTransformControl.Width = 100d;
+        layoutTransformControl.Height = 100d;
+        // TODO: Preview
+        return layoutTransformControl;
+    }
+
+    public override Control CreateControl()
+    {
+        var layoutTransformControl = new LayoutTransformControl();
+        layoutTransformControl.Width = 100d;
+        layoutTransformControl.Height = 100d;
+        // TODO: Content
+        DragSettings.SetIsDropArea(layoutTransformControl, true);
+        DragSettings.SetSnapToGrid(layoutTransformControl, false);
+        return layoutTransformControl;
+    }
+
+    public override void UpdatePreview(Control control, bool isPointerOver)
+    {
+        if (control is not LayoutTransformControl layoutTransformControl)
+        {
+            return;
+        }
+
+        // TODO: Preview
+    }
+}
