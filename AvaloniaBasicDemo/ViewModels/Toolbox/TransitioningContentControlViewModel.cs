c0ce6a1d5c579fe9a9d4abd19c4ea7f1a65452e3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-03 11:44:34 +0200

    Create TransitioningContentControlViewModel.cs

---
+++ AvaloniaBasicDemo/ViewModels/Toolbox/TransitioningContentControlViewModel.cs
@@ -0,0 +1,37 @@
+using Avalonia.Controls;
+using AvaloniaBasicDemo.Behaviors;
+
+namespace AvaloniaBasicDemo.ViewModels.Toolbox;
+
+public class TransitioningContentControlViewModel : DragItemViewModel
+{
+    public override Control CreatePreview()
+    {
+        var transitioningContentControl = new TransitioningContentControl();
+        transitioningContentControl.Width = 100d;
+        transitioningContentControl.Height = 100d;
+        // TODO: Preview
+        return transitioningContentControl;
+    }
+
+    public override Control CreateControl()
+    {
+        var transitioningContentControl = new TransitioningContentControl();
+        transitioningContentControl.Width = 100d;
+        transitioningContentControl.Height = 100d;
+        // TODO: Content
+        DragSettings.SetIsDropArea(transitioningContentControl, true);
+        DragSettings.SetSnapToGrid(transitioningContentControl, false);
+        return transitioningContentControl;
+    }
+
+    public override void UpdatePreview(Control control, bool isPointerOver)
+    {
+        if (control is not TransitioningContentControl transitioningContentControl)
+        {
+            return;
+        }
+
+        // TODO: Preview
+    }
+}
