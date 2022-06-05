53464534faee5f135054d3dbcdaab0560af7e4b6
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-06-05 23:21:52 +0200

    Create LogicalViewModel.cs

---
+++ AvaloniaBasicDemo/ViewModels/LogicalViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.ObjectModel;
+using Avalonia.LogicalTree;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaBasicDemo.ViewModels;
+
+public partial class LogicalViewModel : ObservableObject
+{
+    [ObservableProperty] private string? _name;
+    [ObservableProperty] private bool _isExpanded;
+    [ObservableProperty] private ILogical? _logical;
+    [ObservableProperty] private ObservableCollection<LogicalViewModel>? _children;
+}
