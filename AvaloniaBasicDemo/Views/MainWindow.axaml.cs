7dad923a1520e6b37921468b3c33c55484f76343
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-06-05 23:22:09 +0200

    Update MainWindow.axaml.cs

--- AvaloniaBasicDemo/Views/MainWindow.axaml.cs
+++ AvaloniaBasicDemo/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using AvaloniaBasicDemo.ViewModels;
 
 namespace AvaloniaBasicDemo.Views;
 
@@ -7,5 +9,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        Opened += OnOpened;
+    }
+
+    private void OnOpened(object? sender, EventArgs e)
+    {
+        if (DataContext is MainViewModel mainViewModel)
+        {
+            mainViewModel.UpdateLogicalTreeSource(DropAreaCanvas);
+        }
     }
 }
