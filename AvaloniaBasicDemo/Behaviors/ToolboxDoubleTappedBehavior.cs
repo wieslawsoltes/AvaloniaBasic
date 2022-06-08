971393659785a5047ccd88bf3e0c6d015147fc74
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-08 15:00:12 +0200

    Create ToolboxDoubleTappedBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/ToolboxDoubleTappedBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using AvaloniaBasicDemo.Model;
+using AvaloniaBasicDemo.ViewModels;
+
+namespace AvaloniaBasicDemo.Behaviors;
+
+public class ToolboxDoubleTappedBehavior : DoubleTappedBehavior<Control>
+{
+    public static readonly StyledProperty<Canvas?> TargetCanvasProperty = 
+        AvaloniaProperty.Register<ToolboxDragBehavior, Canvas?>(nameof(TargetCanvas));
+
+    [ResolveByName]
+    public Canvas? TargetCanvas
+    {
+        get => GetValue(TargetCanvasProperty);
+        set => SetValue(TargetCanvasProperty, value);
+    }
+
+    protected override void OnDoubleTapped(object? sender, RoutedEventArgs e)
+    {
+        if (TargetCanvas is null)
+        {
+            return;
+        }
+
+        if (AssociatedObject?.DataContext is IDragItem item)
+        {
+            var control = item.CreateControl();
+
+            ToolboxDragBehavior.AddControl(control, TargetCanvas, new Point(0d, 0d));
+
+            if (TargetCanvas?.DataContext is MainViewModel mainViewModel)
+            {
+                mainViewModel.Tree.UpdateLogicalTree(TargetCanvas);
+            }
+        }
+    }
+}
