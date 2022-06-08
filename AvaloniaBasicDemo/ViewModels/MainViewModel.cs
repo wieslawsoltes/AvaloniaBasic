7411d6d3f3cc5415d765ceea08fad9c4cf7e3801
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-08 13:08:40 +0200

    Update MainViewModel.cs

--- AvaloniaBasicDemo/ViewModels/MainViewModel.cs
+++ AvaloniaBasicDemo/ViewModels/MainViewModel.cs
@@ -6,10 +6,14 @@ public partial class MainViewModel : ObservableObject
 {
     [ObservableProperty] private ToolboxViewModel _toolbox;
     [ObservableProperty] private TreeViewModel _tree;
+    [ObservableProperty] private DragSettingsViewModel _dragSettings;
+    [ObservableProperty] private GridSettingsViewModel _gridSettings;
 
     public MainViewModel()
     {
         _toolbox = new ToolboxViewModel();
         _tree = new TreeViewModel();
+        _dragSettings = new DragSettingsViewModel();
+        _gridSettings = new GridSettingsViewModel();
     }
 }
