80d456d7e71c68a7edf92fd9802fe4b4f7f40bfb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-08 15:00:07 +0200

    Create DoubleTappedBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/DoubleTappedBehavior.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace AvaloniaBasicDemo.Behaviors;
+
+public abstract class DoubleTappedBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<DoubleTappedBehavior<T>, RoutingStrategies>(nameof(RoutingStrategies), RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        if (AssociatedObject is { })
+        {
+            AssociatedObject.AddHandler(Gestures.DoubleTappedEvent, DoubleTapped, RoutingStrategies);
+        } 
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        if (AssociatedObject is { })
+        {
+            AssociatedObject.RemoveHandler(Gestures.DoubleTappedEvent, DoubleTapped);
+        }
+    }
+
+    private void DoubleTapped(object? sender, RoutedEventArgs e)
+    {
+        OnDoubleTapped(sender, e);
+    }
+
+    protected virtual void OnDoubleTapped(object? sender, RoutedEventArgs e)
+    {
+    }
+}
