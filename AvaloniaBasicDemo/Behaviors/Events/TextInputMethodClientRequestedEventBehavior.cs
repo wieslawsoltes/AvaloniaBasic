d466f855de3fc8e6b9dfdbc23d98f4c4daec1aa2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:55:44 +0200

    Create TextInputMethodClientRequestedEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/TextInputMethodClientRequestedEventBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.TextInput;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class TextInputMethodClientRequestedEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<TextInputMethodClientRequestedEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(InputElement.TextInputMethodClientRequestedEvent, TextInputMethodClientRequested, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.TextInputMethodClientRequestedEvent, TextInputMethodClientRequested);
+    }
+
+    private void TextInputMethodClientRequested(object? sender, TextInputMethodClientRequestedEventArgs e)
+    {
+        OnTextInputMethodClientRequested(sender, e);
+    }
+
+    protected virtual void OnTextInputMethodClientRequested(object? sender, TextInputMethodClientRequestedEventArgs e)
+    {
+    }
+}
