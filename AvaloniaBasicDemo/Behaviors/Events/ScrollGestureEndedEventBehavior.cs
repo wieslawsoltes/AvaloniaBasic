f8a492d0faf3fc738c8bf8aba64f568f012750ff
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:17:05 +0200

    Create ScrollGestureEndedEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/ScrollGestureEndedEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class ScrollGestureEndedEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<ScrollGestureEndedEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(Gestures.ScrollGestureEndedEvent, ScrollGestureEnded, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(Gestures.ScrollGestureEndedEvent, ScrollGestureEnded);
+    }
+
+    private void ScrollGestureEnded(object? sender, ScrollGestureEventArgs e)
+    {
+        OnScrollGestureEnded(sender, e);
+    }
+
+    protected virtual void OnScrollGestureEnded(object? sender, ScrollGestureEventArgs e)
+    {
+    }
+}
