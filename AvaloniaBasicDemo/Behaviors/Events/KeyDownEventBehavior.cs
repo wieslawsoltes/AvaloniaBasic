e21cb9fd2464ed009123ebc665a827004c13e5d6
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:53:16 +0200

    Create KeyDownEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/KeyDownEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class KeyDownEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<KeyDownEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(InputElement.KeyDownEvent, KeyDown, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.KeyDownEvent, KeyDown);
+    }
+
+    private void KeyDown(object? sender, KeyEventArgs e)
+    {
+        OnKeyDown(sender, e);
+    }
+
+    protected virtual void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+    }
+}
