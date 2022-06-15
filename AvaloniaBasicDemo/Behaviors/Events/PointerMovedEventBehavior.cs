ef124ae7f1cac7d4775449532799ff24b79efff7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:55:23 +0200

    Create PointerMovedEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/PointerMovedEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class PointerMovedEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<PointerMovedEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(InputElement.PointerMovedEvent, PointerMoved, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.PointerMovedEvent, PointerMoved);
+    }
+
+    private void PointerMoved(object? sender, PointerEventArgs e)
+    {
+        OnPointerMoved(sender, e);
+    }
+
+    protected virtual void OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+    }
+}
