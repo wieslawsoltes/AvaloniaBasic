e7a43dd924bfa7db8b8b04752aca2477fec47318
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:55:46 +0200

    Create TextInputOptionsQueryEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/TextInputOptionsQueryEventBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.TextInput;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class TextInputOptionsQueryEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<TextInputOptionsQueryEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Tunnel | RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(InputElement.TextInputOptionsQueryEvent, TextInputOptionsQuery, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.TextInputOptionsQueryEvent, TextInputOptionsQuery);
+    }
+
+    private void TextInputOptionsQuery(object? sender, TextInputOptionsQueryEventArgs e)
+    {
+        OnTextInputOptionsQuery(sender, e);
+    }
+
+    protected virtual void OnTextInputOptionsQuery(object? sender, TextInputOptionsQueryEventArgs e)
+    {
+    }
+}
