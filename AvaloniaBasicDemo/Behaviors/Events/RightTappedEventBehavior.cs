239a960d191b1edb528527d3fd0b2814e9853664
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:09:13 +0200

    Create RightTappedEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/RightTappedEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class RightTappedEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<RightTappedEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(Gestures.RightTappedEvent, RightTapped, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(Gestures.RightTappedEvent, RightTapped);
+    }
+
+    private void RightTapped(object? sender, RoutedEventArgs e)
+    {
+        OnRightTapped(sender, e);
+    }
+
+    protected virtual void OnRightTapped(object? sender, RoutedEventArgs e)
+    {
+    }
+}
