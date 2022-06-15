38fcb0aa96cde5bbefeffa717c0202db60c421c3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:55:20 +0200

    Create PointerLeaveEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/PointerLeaveEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class PointerLeaveEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<PointerLeaveEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Direct);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(InputElement.PointerLeaveEvent, PointerLeave, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.PointerLeaveEvent, PointerLeave);
+    }
+
+    private void PointerLeave(object? sender, PointerEventArgs e)
+    {
+        OnPointerLeave(sender, e);
+    }
+
+    protected virtual void OnPointerLeave(object? sender, PointerEventArgs e)
+    {
+    }
+}
