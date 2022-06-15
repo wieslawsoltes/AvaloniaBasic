5859bef86cf18c18cf30522428bd9a70e4e8e698
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:53:27 +0200

    Create LostFocusEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/LostFocusEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class LostFocusEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<LostFocusEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(InputElement.LostFocusEvent, LostFocus, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.LostFocusEvent, LostFocus);
+    }
+
+    private void LostFocus(object? sender, RoutedEventArgs e)
+    {
+        OnLostFocus(sender, e);
+    }
+
+    protected virtual void OnLostFocus(object? sender, RoutedEventArgs e)
+    {
+    }
+}
