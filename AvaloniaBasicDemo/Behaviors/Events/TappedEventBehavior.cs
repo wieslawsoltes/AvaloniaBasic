d38519a4ea11262ce82f06cdeab5a7911ad22190
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-06-15 13:09:10 +0200

    Create TappedEventBehavior.cs

---
+++ AvaloniaBasicDemo/Behaviors/Events/TappedEventBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Events;
+
+public abstract class TappedEventBehavior<T> : Behavior<T> where T : Control
+{
+    public static readonly StyledProperty<RoutingStrategies> RoutingStrategiesProperty = 
+        AvaloniaProperty.Register<TappedEventBehavior<T>, RoutingStrategies>(
+            nameof(RoutingStrategies),
+            RoutingStrategies.Bubble);
+
+    public RoutingStrategies RoutingStrategies
+    {
+        get => GetValue(RoutingStrategiesProperty);
+        set => SetValue(RoutingStrategiesProperty, value);
+    }
+
+    protected override void OnAttachedToVisualTree()
+    {
+        AssociatedObject?.AddHandler(Gestures.TappedEvent, Tapped, RoutingStrategies);
+    }
+
+    protected override void OnDetachedFromVisualTree()
+    {
+        AssociatedObject?.RemoveHandler(Gestures.TappedEvent, Tapped);
+    }
+
+    private void Tapped(object? sender, RoutedEventArgs e)
+    {
+        OnTapped(sender, e);
+    }
+
+    protected virtual void OnTapped(object? sender, RoutedEventArgs e)
+    {
+    }
+}
