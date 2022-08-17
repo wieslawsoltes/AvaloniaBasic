ae1bb414757bfa86888ac614134b1f82ce1d2bf8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-17 23:28:14 +0200

    Update DragItemViewModel.cs

--- AvaloniaBasic.Toolbox.Base/ViewModels/DragItemViewModel.cs
+++ AvaloniaBasic.Toolbox.Base/ViewModels/DragItemViewModel.cs
@@ -7,6 +7,7 @@ namespace AvaloniaBasic.ViewModels;
 public abstract partial class DragItemViewModel : ObservableObject, IDragItem
 {
     [ObservableProperty] private string? _name;
+    [ObservableProperty] private string? _group;
     [ObservableProperty] private string? _icon;
     [ObservableProperty] private bool _isExpanded;
 
