4370c2a1aa4aadfcaf14626d9b7eab5dbd4892fc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-17 23:28:00 +0200

    Update ToolboxGroupViewModel.cs

--- AvaloniaBasic.Toolbox.Base/ViewModels/ToolboxGroupViewModel.cs
+++ AvaloniaBasic.Toolbox.Base/ViewModels/ToolboxGroupViewModel.cs
@@ -7,6 +7,7 @@ namespace AvaloniaBasic.ViewModels;
 public partial class ToolboxGroupViewModel : ObservableObject, IToolBoxItem
 {
     [ObservableProperty] private string? _name;
+    [ObservableProperty] private string? _group;
     [ObservableProperty] private string? _icon;
     [ObservableProperty] private bool _isExpanded = true;
     [ObservableProperty] private ObservableCollection<IDragItem>? _items;
