208d754a624c5790289c8209783d49e0c670b67d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-17 23:42:41 +0200

    Update WrapPanelViewModel.cs

--- AvaloniaBasic.Toolbox/Controls/WrapPanelViewModel.cs
+++ AvaloniaBasic.Toolbox/Controls/WrapPanelViewModel.cs
@@ -3,7 +3,7 @@ using Avalonia.Media;
 
 namespace AvaloniaBasic.ViewModels.Toolbox;
 
-public class WrapPanelViewModel : ToolBoxItemViewModel
+public class WrapPanelViewModel : ToolboxItemViewModel
 {
     public WrapPanelViewModel()
     {
