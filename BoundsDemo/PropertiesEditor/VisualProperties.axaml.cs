a31ba8d1618d3aa4bda4df82436ad281ad4d40a3
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-12-25 14:40:01 +0100

    Add Opacity and IsVisible

--- BoundsDemo/PropertiesEditor/VisualProperties.axaml.cs
+++ BoundsDemo/PropertiesEditor/VisualProperties.axaml.cs
@@ -1,12 +1,14 @@
+using System.Globalization;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace BoundsDemo;
 
 public partial class VisualProperties : UserControl
 {
     public static readonly StyledProperty<Visual?> SelectedProperty = 
-        AvaloniaProperty.Register<LayoutableProperties, Visual?>(nameof(Selected));
+        AvaloniaProperty.Register<VisualProperties, Visual?>(nameof(Selected));
 
     private bool _isUpdating;
 
@@ -27,10 +29,57 @@ public partial class VisualProperties : UserControl
         
         if (Selected is { } visual)
         {
-            // TODO:
+            SetIsVisible(visual.IsVisible);
+
+            TextBoxOpacity.Text = visual.Opacity.ToString(CultureInfo.InvariantCulture);
         }
 
         _isUpdating = false;
+    }
+
+    private void SetIsVisible(bool isVisible)
+    {
+        SvgIsVisibleTrue.IsVisible = isVisible;
+        SvgIsVisibleFalse.IsVisible = !isVisible;
+    }
+
+    private void ButtonIsVisible_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Selected is { } visual)
+        {
+            visual.IsVisible = !visual.IsVisible;
+
+            var isVisible = visual.IsVisible;
+            SetIsVisible(isVisible);
+
+            if (DataContext is ToolBoxViewModel toolBoxViewModel)
+            {
+                toolBoxViewModel.UpdatePropertyValue(visual as Control, "IsVisible", $"{isVisible}");
+            }
+        }
+    }
+
+    private void TextBoxOpacity_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (_isUpdating)
+        {
+            return;
+        }
+
+        if (Selected is { } visual)
+        {
+            var text = TextBoxOpacity.Text;
+            var result = double.TryParse(text, CultureInfo.InvariantCulture, out var value);
+            if (result)
+            {
+                visual.Opacity = value;
+            
+                if (DataContext is ToolBoxViewModel toolBoxViewModel)
+                {
+                    toolBoxViewModel.UpdatePropertyValue(visual as Control, "Opacity", text);
+                }
+            }
+        }
     }
 }
 
