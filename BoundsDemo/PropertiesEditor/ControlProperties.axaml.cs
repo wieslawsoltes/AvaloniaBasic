16f37d63cf217cb2432eb1b57396bd486c0e7af0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-12-26 17:12:06 +0100

    Update

---
+++ BoundsDemo/PropertiesEditor/ControlProperties.axaml.cs
@@ -0,0 +1,36 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace BoundsDemo;
+
+public partial class ControlProperties : UserControl
+{
+    public static readonly StyledProperty<Visual?> SelectedProperty = 
+        AvaloniaProperty.Register<ControlProperties, Visual?>(nameof(Selected));
+
+    private bool _isUpdating;
+
+    public Visual? Selected
+    {
+        get => GetValue(SelectedProperty);
+        set => SetValue(SelectedProperty, value);
+    }
+
+    public ControlProperties()
+    {
+        InitializeComponent();
+    }
+
+    public void UpdateControlProperties()
+    {
+        _isUpdating = true;
+        
+        if (Selected is Control control)
+        {
+            // TODO:
+        }
+
+        _isUpdating = false;
+    }
+}
+
---
+++ BoundsDemo/PropertiesEditor/InputElementProperties.axaml.cs
@@ -0,0 +1,37 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace BoundsDemo;
+
+public partial class InputElementProperties : UserControl
+{
+    public static readonly StyledProperty<Visual?> SelectedProperty = 
+        AvaloniaProperty.Register<InputElementProperties, Visual?>(nameof(Selected));
+
+    private bool _isUpdating;
+
+    public Visual? Selected
+    {
+        get => GetValue(SelectedProperty);
+        set => SetValue(SelectedProperty, value);
+    }
+
+    public InputElementProperties()
+    {
+        InitializeComponent();
+    }
+
+    public void UpdateInputElementProperties()
+    {
+        _isUpdating = true;
+        
+        if (Selected is InputElement inputElement)
+        {
+            // TODO:
+        }
+
+        _isUpdating = false;
+    }
+}
+
