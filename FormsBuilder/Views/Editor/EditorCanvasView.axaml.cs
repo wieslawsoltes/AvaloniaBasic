46a2986376c5dd575640b3cbd4a03d418b259252
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-11 20:18:03 +0100

    Update EditorCanvasView.axaml.cs

--- FormsBuilder/Views/Editor/EditorCanvasView.axaml.cs
+++ FormsBuilder/Views/Editor/EditorCanvasView.axaml.cs
@@ -28,7 +28,7 @@ public partial class EditorCanvasView : UserControl
         if (DataContext is MainViewViewModel mainViewModel)
         {
             mainViewModel.XamlEditorViewModel.CanvasViewModel = new CanvasViewModel(OverlayView);
-            mainViewModel.XamlEditorViewModel.CanvasViewModel.AttachHost(this, RootPanel);
+            mainViewModel.XamlEditorViewModel.CanvasViewModel.AttachHost(this, RootPanel, GridLines);
 
             // TODO:
             mainViewModel.NewCommand.Execute(null);
