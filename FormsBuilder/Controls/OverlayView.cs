1bab8b98f651b0aee477836af18cf91a99a80f19
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-14 18:45:50 +0100

    Update OverlayView.cs

--- FormsBuilder/Controls/OverlayView.cs
+++ FormsBuilder/Controls/OverlayView.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
@@ -9,8 +8,134 @@ using Avalonia.VisualTree;
 
 namespace FormsBuilder;
 
+public abstract class Renderer
+{
+    public abstract void Render(DrawingContext context);
+}
+
+public class SelectionRenderer : Renderer
+{
+    private readonly MainViewViewModel _mainViewModel;
+
+    public SelectionRenderer(MainViewViewModel mainViewModel)
+    {
+        _mainViewModel = mainViewModel;
+    }
+
+    public override void Render(DrawingContext context)
+    {
+        var selected = _mainViewModel.XamlSelectionViewModel.Selected;
+        var hovered = _mainViewModel.XamlSelectionViewModel.Hovered;
+
+        if (selected.Count > 0)
+        {
+            foreach (var visual in selected)
+            {
+                RenderVisual(visual, context, new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
+                RenderVisualThumbs(visual, context, new ImmutableSolidColorBrush(Colors.White), new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
+            }
+
+            if (hovered is null)
+            {
+                // DrawName(context, selected.GetType().Name);
+            }
+        }
+
+        if (hovered is not null)
+        {
+            RenderVisual(hovered, context, new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
+            // DrawName(context, hovered.GetType().Name);
+        }
+
+        if (_mainViewModel.XamlSelectionViewModel.DrawSelection)
+        {
+            var rect = RectHelper.GetSelectionRect(
+                _mainViewModel.XamlSelectionViewModel.StartPoint,
+                _mainViewModel.XamlSelectionViewModel.EndPoint);
+
+            RenderSelection(
+                rect, 
+                context, 
+                new ImmutableSolidColorBrush(Colors.CornflowerBlue, 0.3), 
+                new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
+        }
+
+        // DrawHelp(context);
+    }
+
+    private static void RenderVisual(Visual visual, DrawingContext context, IPen? pen)
+    {
+        var transformedBounds = visual.GetTransformedBounds();
+        if (transformedBounds is null)
+        {
+            return;
+        }
+
+        using var _ = context.PushTransform(transformedBounds.Value.Transform);
+        context.DrawRectangle(null, pen, transformedBounds.Value.Bounds);
+    }
+
+    private static void RenderVisualThumbs(Visual visual, DrawingContext context, IBrush? brush, IPen? pen)
+    {
+        var transformedBounds = visual.GetTransformedBounds();
+        if (transformedBounds is null)
+        {
+            return;
+        }
+
+        using var _ = context.PushTransform(transformedBounds.Value.Transform);
+        var bounds = transformedBounds.Value.Bounds;
+        var selection = new VisualSelection(bounds, 3);
+        context.DrawRectangle(brush, pen, selection.TopLeft);
+        context.DrawRectangle(brush, pen, selection.TopRight);
+        context.DrawRectangle(brush, pen, selection.BottomLeft);
+        context.DrawRectangle(brush, pen, selection.BottomRight);
+        context.DrawRectangle(brush, pen, selection.Left);
+        context.DrawRectangle(brush, pen, selection.Right);
+        context.DrawRectangle(brush, pen, selection.Top);
+        context.DrawRectangle(brush, pen, selection.Bottom);
+    }
+  
+    private static void RenderSelection(Rect rect, DrawingContext context, IBrush? brush, IPen? pen)
+    {
+        context.DrawRectangle(brush, pen, rect);
+    }
+
+    /*
+    private void DrawHelp(DrawingContext context)
+    {
+        var helpText = $"[V] [L] Mode: {HitTestMode}, [H] Toggle HitTest, [R] Toggle Reverse Order";
+
+        var formattedTextMode = new FormattedText(
+            helpText, 
+            CultureInfo.CurrentCulture, 
+            FlowDirection.LeftToRight, 
+            Typeface.Default, 
+            12, 
+            Brushes.CornflowerBlue);
+
+        context.DrawText(formattedTextMode, new Point(10, Bounds.Height - 12 - 5));
+    }
+
+    private void DrawName(DrawingContext context, string name)
+    {
+        var formattedText = new FormattedText(
+            name, 
+            CultureInfo.CurrentCulture, 
+            FlowDirection.LeftToRight, 
+            Typeface.Default, 
+            12, 
+            Brushes.CornflowerBlue);
+
+        context.DrawText(formattedText, new Point(Bounds.Width - formattedText.Width - 10, Bounds.Height - 12 - 5));
+    }
+    */
+}
+
 public class OverlayView : Decorator
 {
+    private SelectionRenderer? _selectionRenderer;
+    
     public OverlayView()
     {
         Child = new Canvas();
@@ -88,117 +213,25 @@ public class OverlayView : Decorator
 
         return null;
     }
-    
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        
+        if (DataContext is MainViewViewModel mainViewModel)
+        {
+            _selectionRenderer = new SelectionRenderer(mainViewModel);
+        }
+        else
+        {
+            _selectionRenderer = null;
+        }
+    }
+
     public override void Render(DrawingContext context)
     {
         base.Render(context);
 
-        if (DataContext is not MainViewViewModel mainViewModel)
-        {
-            return;
-        }
-
-        var selected = mainViewModel.XamlSelectionViewModel.Selected;
-        var hovered = mainViewModel.XamlSelectionViewModel.Hovered;
-
-        if (selected.Count > 0)
-        {
-            foreach (var visual in selected)
-            {
-                RenderVisual(visual, context, new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
-                RenderVisualThumbs(visual, context, new ImmutableSolidColorBrush(Colors.White), new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
-            }
-
-            if (hovered is null)
-            {
-                // DrawName(context, selected.GetType().Name);
-            }
-        }
-
-        if (hovered is not null)
-        {
-            RenderVisual(hovered, context, new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
-            // DrawName(context, hovered.GetType().Name);
-        }
-
-        if (mainViewModel.XamlSelectionViewModel.DrawSelection)
-        {
-            var rect = mainViewModel.XamlSelectionViewModel.GetSelectionRect();
-
-            RenderSelection(
-                rect, 
-                context, 
-                new ImmutableSolidColorBrush(Colors.CornflowerBlue, 0.3), 
-                new ImmutablePen(Colors.CornflowerBlue.ToUInt32()));
-        }
-
-        // DrawHelp(context);
-    }
-
-    private static void RenderVisual(Visual visual, DrawingContext context, IPen? pen)
-    {
-        var transformedBounds = visual.GetTransformedBounds();
-        if (transformedBounds is null)
-        {
-            return;
-        }
-
-        using var _ = context.PushTransform(transformedBounds.Value.Transform);
-        context.DrawRectangle(null, pen, transformedBounds.Value.Bounds);
-    }
-
-    private static void RenderVisualThumbs(Visual visual, DrawingContext context, IBrush? brush, IPen? pen)
-    {
-        var transformedBounds = visual.GetTransformedBounds();
-        if (transformedBounds is null)
-        {
-            return;
-        }
-
-        using var _ = context.PushTransform(transformedBounds.Value.Transform);
-        var bounds = transformedBounds.Value.Bounds;
-        var selection = new VisualSelection(bounds, 3);
-        context.DrawRectangle(brush, pen, selection.TopLeft);
-        context.DrawRectangle(brush, pen, selection.TopRight);
-        context.DrawRectangle(brush, pen, selection.BottomLeft);
-        context.DrawRectangle(brush, pen, selection.BottomRight);
-        context.DrawRectangle(brush, pen, selection.Left);
-        context.DrawRectangle(brush, pen, selection.Right);
-        context.DrawRectangle(brush, pen, selection.Top);
-        context.DrawRectangle(brush, pen, selection.Bottom);
-    }
-    
-    
-    private static void RenderSelection(Rect rect, DrawingContext context, IBrush? brush, IPen? pen)
-    {
-        context.DrawRectangle(brush, pen, rect);
-    }
-
-    private void DrawHelp(DrawingContext context)
-    {
-        var helpText = $"[V] [L] Mode: {HitTestMode}, [H] Toggle HitTest, [R] Toggle Reverse Order";
-
-        var formattedTextMode = new FormattedText(
-            helpText, 
-            CultureInfo.CurrentCulture, 
-            FlowDirection.LeftToRight, 
-            Typeface.Default, 
-            12, 
-            Brushes.CornflowerBlue);
-
-        context.DrawText(formattedTextMode, new Point(10, Bounds.Height - 12 - 5));
-    }
-
-    private void DrawName(DrawingContext context, string name)
-    {
-        var formattedText = new FormattedText(
-            name, 
-            CultureInfo.CurrentCulture, 
-            FlowDirection.LeftToRight, 
-            Typeface.Default, 
-            12, 
-            Brushes.CornflowerBlue);
-
-        context.DrawText(formattedText, new Point(Bounds.Width - formattedText.Width - 10, Bounds.Height - 12 - 5));
+        _selectionRenderer?.Render(context);
     }
 }
