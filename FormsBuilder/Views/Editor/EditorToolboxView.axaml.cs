b929567d738cd4d6851181705bab0bcdfb1085d2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-18 17:04:12 +0100

    Update EditorToolboxView.axaml.cs

--- FormsBuilder/Views/Editor/EditorToolboxView.axaml.cs
+++ FormsBuilder/Views/Editor/EditorToolboxView.axaml.cs
@@ -18,7 +18,10 @@ public partial class EditorToolboxView : UserControl
 
         if (DataContext is MainViewViewModel mainViewModel)
         {
-            _toolboxViewModel = new ToolboxViewModel(this, mainViewModel.OverlayService, mainViewModel.XamlEditor);
+            _toolboxViewModel = new ToolboxViewModel(
+                this, 
+                mainViewModel.OverlayService, 
+                mainViewModel.XamlEditor);
 
             ToolboxListBox.ContainerPrepared += ToolboxListBoxOnContainerPrepared;
             ToolboxListBox.ContainerClearing += ToolboxListBoxOnContainerClearing;
