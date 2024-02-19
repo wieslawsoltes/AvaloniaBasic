9cb6f710bfb6587efde569a6327c8a1c35c6bc29
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-19 07:21:15 +0100

    Update RectangleTool.cs

--- FormsBuilder/Model/Tools/Draw/RectangleTool.cs
+++ FormsBuilder/Model/Tools/Draw/RectangleTool.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 
 namespace FormsBuilder;
 
@@ -37,16 +40,52 @@ public class RectangleTool : Tool
 
     public override void OnPointerReleased(IToolContext context, object? sender, PointerReleasedEventArgs e)
     {
-        if (e.Pointer.Captured is not null && _captured)
+        if (e.Pointer.Captured is null || !_captured)
         {
-            e.Pointer.Capture(null);
-            _captured = false;
+            return;
+        }
+        
+        e.Pointer.Capture(null);
+        _captured = false;
 
-            _endPoint = e.GetPosition(null);
-            _selectionRect = RectHelper.GetSelectionRect(_startPoint, _endPoint);
+        _endPoint = e.GetPosition(null);
+        _selectionRect = RectHelper.GetSelectionRect(_startPoint, _endPoint);
   
-            context.XamlSelection.Selection(_startPoint, _endPoint);
-            context.XamlSelection.ClearSelection();
+        context.XamlSelection.Selection(_startPoint, _endPoint);
+        context.XamlSelection.ClearSelection();
+
+        {
+            var visualRoot = (sender as Control).GetVisualRoot() as Interactive;
+
+            var target = context.XamlEditor.HitTest(visualRoot, e.GetPosition(visualRoot), _ignored);
+            if (target is null)
+            {
+                return;
+            }
+
+            // TODO:
+            // var position = _selectionRect.TopLeft;
+            // var position = e.GetPosition(target);
+
+            var width = _selectionRect.Width;
+            var height = _selectionRect.Height;
+            var position = visualRoot.TranslatePoint(_selectionRect.TopLeft, target).Value;
+
+            var rectangleXamlItem = new XamlItem(name: "Rectangle",
+                id: context.XamlEditor.IdManager.GetNewId(),
+                properties: new Dictionary<string, XamlValue>
+                {
+                    ["Fill"] = (XamlValue) "Blue",
+                },
+                contentProperty: null,
+                childrenProperty: null);
+
+            rectangleXamlItem.Properties["Canvas.Left"] = StringXamlValue.From(position.X);
+            rectangleXamlItem.Properties["Canvas.Top"] = StringXamlValue.From(position.Y);
+            rectangleXamlItem.Properties["Width"] = StringXamlValue.From(width);
+            rectangleXamlItem.Properties["Height"] = StringXamlValue.From(height);
+
+            context.XamlEditor.InsertXamlItem(target, rectangleXamlItem, position);
         }
     }
 
