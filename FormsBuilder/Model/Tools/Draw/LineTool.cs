6857583c2e93cdb3e1e673315a3dd16156973a01
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2024-02-24 09:06:57 +0100

    Update LineTool.cs

--- FormsBuilder/Model/Tools/Draw/LineTool.cs
+++ FormsBuilder/Model/Tools/Draw/LineTool.cs
@@ -28,16 +28,30 @@ public class LineTool : DrawTool
 
     protected override void UpdateXamlItem(Point startPosition, Point endPosition, XamlItem xamlItem)
     {
-        xamlItem.Properties["StartPoint"] = StringXamlValue.From(startPosition);
-        xamlItem.Properties["EndPoint"] = StringXamlValue.From(endPosition);
+        var startPoint = startPosition;
+        var endPoint = endPosition - startPosition;
+
+        xamlItem.Properties["Canvas.Left"] = StringXamlValue.From(startPoint.X);
+        xamlItem.Properties["Canvas.Top"] = StringXamlValue.From(startPoint.Y);
+        xamlItem.Properties["StartPoint"] = StringXamlValue.From(new Point());
+        xamlItem.Properties["EndPoint"] = StringXamlValue.From(endPoint);
+        //xamlItem.Properties["StartPoint"] = StringXamlValue.From(startPosition);
+        //xamlItem.Properties["EndPoint"] = StringXamlValue.From(endPosition);
     }
 
     protected override void UpdateControl(Point startPosition, Point endPosition, Control control)
     {
         if (control is Line line)
         {
-            line.StartPoint = startPosition;
-            line.EndPoint = endPosition;
+            var startPoint = startPosition;
+            var endPoint = endPosition - startPosition;
+
+            Canvas.SetLeft(control, startPoint.X);
+            Canvas.SetTop(control, startPoint.Y);
+            line.StartPoint = new Point();
+            line.EndPoint = endPoint;
+            //line.StartPoint = startPosition;
+            //line.EndPoint = endPosition;
         }
     }
 }
