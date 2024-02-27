6b8d6f4d08254f3f4b80d8dc8b861a67cf2a6705
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-27 16:09:50 +0100

    Add Styles tab

---
+++ FormsBuilder/Views/Editor/EditorStylesView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FormsBuilder;
+
+public partial class EditorStylesView : UserControl
+{
+    public EditorStylesView()
+    {
+        InitializeComponent();
+    }
+}
